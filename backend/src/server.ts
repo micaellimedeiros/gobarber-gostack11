df23e718a29cc1136bfe845357ce572566333e44
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-07 22:32:45 -0300

    added: server file

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🚀️ Server started on port 3333!');
+});
