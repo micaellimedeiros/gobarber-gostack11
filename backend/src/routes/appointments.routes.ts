874eac9b128f020ea2c08d5d02a1cce8ff8d66bc
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-08 21:29:00 -0300

    feat: separating items to the model

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,16 +1,10 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
@@ -27,11 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'The appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
