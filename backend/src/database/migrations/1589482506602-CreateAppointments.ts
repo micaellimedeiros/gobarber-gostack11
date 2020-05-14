24eaf7340689d370533007c8e24e1cdae973e709
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-14 16:12:57 -0300

    added: configs and migration

---
+++ backend/src/database/migrations/1589482506602-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1589482506602 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
