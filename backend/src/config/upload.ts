6efe3c2dd5098de00b296fea620fa4ed501e8891
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-15 19:43:52 -0300

    added: upload archives

---
+++ backend/src/config/upload.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('HEX');
+
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -17,5 +22,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
