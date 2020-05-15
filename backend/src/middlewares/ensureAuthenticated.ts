14d8e99700973e1fc5b3ab7394ac117a80911a77
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-15 19:03:27 -0300

    added: middleware authentication

--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -3,6 +3,12 @@ import { verify } from 'jsonwebtoken';
 
 import authConfig from '../config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -19,7 +25,11 @@ export default function ensureAuthenticated(
   try {
     const decoded = verify(token, authConfig.jwt.secret);
 
-    console.log(decoded);
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
 
     return next();
   } catch {
