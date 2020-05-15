8c2bce457768214d25ff4cd0cd67a943083ba992
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-15 14:24:29 -0300

    added: session routes

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
   try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    await authenticateUser.execute({
+      email,
+      password,
+    });
+
     return response.json({ ok: true });
   } catch (err) {
     return response.status(400).json({ error: err.message });
