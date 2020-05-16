f354c68af7675b6d7879581b1aedefb76fef81a3
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-16 16:47:43 -0300

    added: global error handling

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -20,22 +20,18 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppoinment = new CreateAppointmentService();
+  const createAppoinment = new CreateAppointmentService();
 
-    const appointment = await createAppoinment.execute({
-      date: parsedDate,
-      provider_id,
-    });
+  const appointment = await createAppoinment.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
 /* eslint-disable no-console */
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
+
 import './database';
 
 const app = express();
@@ -10,6 +14,20 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀️ Server started on port 3333!');
