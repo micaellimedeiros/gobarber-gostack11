31ec05d6999c753175116e859a630ec640901ed5
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-16 15:23:49 -0300

    added: updating avatar

---
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,35 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne(user_id);
+    if (!user) {
+      throw new Error('Only authenticated users can change the avatar');
+    }
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+    user.avatar = avatarFilename;
+
+    await userRepository.save(user);
+    return user;
+  }
+}
+export default UpdateUserAvatarService;
