d7223deaa8dc842d9fab3aa62467b7551921a778
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-16 20:03:44 -0300

    feat: fix errors

--- backend/src/database/migrations/1589580302295-AddAvatarFieldToUsers.ts
+++ backend/src/database/migrations/1589580302295-AddAvatarFieldToUsers.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-export default class AddAvatarFieldToUsers1587149949398
+export default class AddAvatarFieldToUsers1589580302295
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
