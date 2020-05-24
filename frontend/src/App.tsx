f62be065c4de0cccd30d68a7f961065a0aefd71c
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-24 09:54:01 -0300

    added: components to app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+import SignIn from './pages/SignIn';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
