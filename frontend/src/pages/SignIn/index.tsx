9ee69c23073c4e6fcf00324702b6980794d43225
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-24 21:22:49 -0300

    feat: insulating components

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
-import logoImg from '../../assets/logo.svg';
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Content>
-      <img src={logoImg} alt="GoBarber" />
+      <img src={logo} alt="GoBarber" />
 
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
-      <a href="login">
+      <a href="dwa">
         <FiLogIn />
         Criar conta
       </a>
