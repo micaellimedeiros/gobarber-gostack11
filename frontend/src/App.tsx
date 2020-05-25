fd1e855aae8cbab68de9a693dfd883facd459fa9
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-24 21:47:10 -0300

    added: new component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    {/* <SignIn /> */}
+    <SignUp />
     <GlobalStyle />
   </>
 );
