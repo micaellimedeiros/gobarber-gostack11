abf68c9e9b064771e2d851557196f419571c4aaf
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-27 10:52:50 -0300

    added: unform

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
@@ -8,12 +9,25 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
-    <input {...rest} />
-  </Container>
-);
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      {Icon && <Icon size={20} />}
+
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+    </Container>
+  );
+};
 
 export default Input;
