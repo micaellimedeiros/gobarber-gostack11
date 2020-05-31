5e0a865b30cb8410a461c4bb8fd3c886acad0e54
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-30 21:53:11 -0300

    added: local storage

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -20,7 +20,8 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { user, signIn } = useContext(AuthContext);
+  console.log(user);
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
