8176c54a438156b32ed1c190fe588f5327903c2c
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-06-01 21:30:41 -0300

    added: toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+import ToastContainer from './components/ToastContainer';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
-
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
