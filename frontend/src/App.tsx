c08f05118746a2a743fc551cf2317bd2adda2744
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-06-07 22:11:55 -0300

    added: new hook of toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,14 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
-import ToastContainer from './components/ToastContainer';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
-    <ToastContainer />
+    </AppProvider>
     <GlobalStyle />
   </>
 );
