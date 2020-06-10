1aa1bdeef4183eb39bbfd3eb42345e15d01dba68
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-06-10 14:31:32 -0300

    add: routes config and animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
-  <>
+  <Router>
     <AppProvider>
-      <SignIn />
+      <Routes />
     </AppProvider>
+
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
