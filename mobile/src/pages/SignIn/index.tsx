ab5148abb91d294c1b6d6edf7452df79447015d3
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-06-14 21:48:20 -0300

    add: navigation

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return <View />;
+};
+
+export default SignIn;
